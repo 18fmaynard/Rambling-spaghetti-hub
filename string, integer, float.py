2ada7371cc0e8240817243aada468400aad2d8e0
Finn Maynard <18fmaynard@holyrood.bep.ac>
2023-09-18 14:41:22 +0100

    string, integer, float

---
+++ string, integer, float.py
@@ -0,0 +1,4 @@
+name = str(input("What is your name?"))
+age = int(input("How old are you?"))
+height = float(input("How tall are you in metres?"))
+print("You are called ", name, " and you are", age, " years old and", age, "m tall")
