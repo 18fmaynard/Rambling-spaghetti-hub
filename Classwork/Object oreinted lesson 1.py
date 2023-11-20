52e534013033a6641c91feb0dc550333616326b7
Finn Maynard <18fmaynard@holyrood.bep.ac>
2023-11-20 14:45:36 +0000

    class and attributes

---
+++ Classwork/Object oreinted lesson 1.py
@@ -0,0 +1,9 @@
+class human():
+    
+    def __init__(self, name = "no name", age = 0, nationality = "no nationality",
+                 gender = "no gengder"):
+        
+        self.name = name
+        self.age = age
+        self.nationality = nationality
+        self.gender = gender
